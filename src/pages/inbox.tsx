dd9b1ca4bf9a9fea4a9e9ce3efb111356b1c19bf
marl-Am <marl-Am@users.noreply.github.com>
2023-07-14 04:02:24 -0400

    Added some route security

--- src/pages/inbox.tsx
+++ src/pages/inbox.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { useUser } from "@clerk/nextjs";
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Inbox({}) {
   const user = useUser();
@@ -9,18 +10,45 @@ export default function Inbox({}) {
   if (!user) {
     router
       .push("/")
-      .then(() => {console.log("Unauthorized Access. Redirected to home page");})
+      .then(() => {
+        console.log("Unauthorized Access. Redirected to home page");
+      })
       .catch((e) => console.error(e));
     return null;
   }
 
   return (
     <>
-      <Head>
-        <title>Inbox</title>
-      </Head>
-      <div className="flex min-h-screen flex-col items-center justify-center">
-        <h1 className="mt-2 text-3xl text-black">Inbox</h1>
+      <div>
+        {user.isSignedIn && (
+          <div>
+            <Head>
+              <title>Inbox</title>
+            </Head>
+            <div className="flex min-h-screen flex-col items-center justify-center">
+              <h1 className="mt-2 text-3xl text-black">Inbox</h1>
+            </div>
+          </div>
+        )}
+
+        {!user.isSignedIn && (
+          <div>
+            <Head>
+              <title>Access Forbidden</title>
+            </Head>
+            <div className="flex min-h-screen flex-col items-center justify-center">
+              <h1 className="mt-2 text-3xl text-black">
+                Access to the requested resource is forbidden.
+              </h1>
+              <Link
+                href="/"
+                className="mt-4 text-indigo-600 hover:text-indigo-800"
+              >
+                Go back to Home
+              </Link>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
--- src/pages/orders.tsx
+++ src/pages/orders.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { useUser } from "@clerk/nextjs";
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Orders({}) {
   const user = useUser();
@@ -17,11 +18,36 @@ export default function Orders({}) {
 
   return (
     <>
-      <Head>
-        <title>Orders</title>
-      </Head>
-      <div className="flex min-h-screen flex-col items-center justify-center">
-        <h1 className="mt-2 text-3xl text-black">Orders</h1>
+      <div>
+        {user.isSignedIn && (
+          <div>
+            <Head>
+              <title>Orders</title>
+            </Head>
+            <div className="flex min-h-screen flex-col items-center justify-center">
+              <h1 className="mt-2 text-3xl text-black">Orders</h1>
+            </div>
+          </div>
+        )}
+
+        {!user.isSignedIn && (
+          <div>
+            <Head>
+              <title>Access Forbidden</title>
+            </Head>
+            <div className="flex min-h-screen flex-col items-center justify-center">
+              <h1 className="mt-2 text-3xl text-black">
+                Access to the requested resource is forbidden.
+              </h1>
+              <Link
+                href="/"
+                className="mt-4 text-indigo-600 hover:text-indigo-800"
+              >
+                Go back to Home
+              </Link>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
