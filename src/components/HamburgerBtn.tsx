e4248d34b26366921760d7526f9a6e3414911a1f
marl-Am <marl-Am@users.noreply.github.com>
2023-07-29 21:29:08 -0400

    Added dropdown class

--- src/components/HamburgerBtn.tsx
+++ src/components/HamburgerBtn.tsx
@@ -34,91 +34,89 @@ export default function HamburgerBtn() {
 
   return (
     <>
-      <div className="dropdown relative gap-2">
-        <button
-          className="group z-10 flex h-10 w-10 flex-col items-center justify-center rounded border-2 border-black"
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          <div
-            className={`${genericHamburgerLine} ${
-              isOpen
-                ? "translate-y-3 rotate-45 opacity-50 group-hover:opacity-100"
-                : "opacity-50 group-hover:opacity-100"
-            }`}
-          />
-          <div
-            className={`${genericHamburgerLine} ${
-              isOpen ? "opacity-0" : "opacity-50 group-hover:opacity-100"
-            }`}
-          />
-          <div
-            className={`${genericHamburgerLine} ${
-              isOpen
-                ? "-translate-y-3 -rotate-45 opacity-50 group-hover:opacity-100"
-                : "opacity-50 group-hover:opacity-100"
-            }`}
-          />
-        </button>
-        {isOpen && (
-          <ul
-            tabIndex={0}
-            className="dropdown-content menu rounded-box menu-sm z-[1] mt-3 w-52 bg-base-100 p-2 shadow"
+      <div className="relative gap-2">
+        <div className="dropdown">
+          <button
+            className="group z-10 flex h-10 w-10 flex-col items-center justify-center rounded border-2 border-black"
+            onClick={() => setIsOpen(!isOpen)}
           >
-            <Link
-              href="/"
-              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-              
+            <div
+              className={`${genericHamburgerLine} ${
+                isOpen
+                  ? "translate-y-3 rotate-45 opacity-50 group-hover:opacity-100"
+                  : "opacity-50 group-hover:opacity-100"
+              }`}
+            />
+            <div
+              className={`${genericHamburgerLine} ${
+                isOpen ? "opacity-0" : "opacity-50 group-hover:opacity-100"
+              }`}
+            />
+            <div
+              className={`${genericHamburgerLine} ${
+                isOpen
+                  ? "-translate-y-3 -rotate-45 opacity-50 group-hover:opacity-100"
+                  : "opacity-50 group-hover:opacity-100"
+              }`}
+            />
+          </button>
+          {isOpen && (
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu rounded-box menu-sm z-[1] mt-3 w-52 bg-base-100 p-2 shadow"
             >
-              Home
-            </Link>
+              <Link
+                href="/"
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              >
+                Home
+              </Link>
 
-            {user.isSignedIn && (
-              <>
-                <Link
-                  href="/inbox"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  
-                >
-                  Inbox
-                </Link>
-                <Link
-                  href="/orders"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  
-                >
-                  Orders
-                </Link>
-                <Link
-                  href="/user_details"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  
-                >
-                  User Details
-                </Link>
-              </>
-            )}
-            <Link
-              href="/view_order"
-              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-            >
-              View Order
-            </Link>
-            <button
-              className="block px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100 lg:hidden"
-              onClick={() => dialogRef.current?.showModal()}
-            >
-              Search
-            </button>
+              {user.isSignedIn && (
+                <>
+                  <Link
+                    href="/inbox"
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                  >
+                    Inbox
+                  </Link>
+                  <Link
+                    href="/orders"
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                  >
+                    Orders
+                  </Link>
+                  <Link
+                    href="/user_details"
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                  >
+                    User Details
+                  </Link>
+                </>
+              )}
+              <Link
+                href="/view_order"
+                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              >
+                View Order
+              </Link>
+              <button
+                className="block px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100 lg:hidden"
+                onClick={() => dialogRef.current?.showModal()}
+              >
+                Search
+              </button>
 
-            {!user.isSignedIn && (
-              <SignInButton mode="modal">
-                <button className="block px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100 lg:hidden">
-                  Sign In
-                </button>
-              </SignInButton>
-            )}
-          </ul>
-        )}
+              {!user.isSignedIn && (
+                <SignInButton mode="modal">
+                  <button className="block px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100 lg:hidden">
+                    Sign In
+                  </button>
+                </SignInButton>
+              )}
+            </ul>
+          )}
+        </div>
       </div>
 
       {/* Search Modal */}
